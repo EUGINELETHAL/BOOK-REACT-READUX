1e699846f93cfb29c19d7366efdd5731c2ffe700
EUGINELETHAL <ochungeugine@gmail.com>
2020-02-02 02:59:17 +0300

    deployed to production

---
+++ build/precache-manifest.cc2481057f9f73075bce070cf2295abc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9f40c7284e7219a573b8b9d38a5b813",
+    "url": "/BOOK-REACT-READUX/index.html"
+  },
+  {
+    "revision": "9f0eafda2f0415ef85c5",
+    "url": "/BOOK-REACT-READUX/static/css/main.8f6fe99b.chunk.css"
+  },
+  {
+    "revision": "3a3119aefc928dffa0cd",
+    "url": "/BOOK-REACT-READUX/static/js/2.28e9ce1c.chunk.js"
+  },
+  {
+    "revision": "783efe56c297789f679b639c509e9a30",
+    "url": "/BOOK-REACT-READUX/static/js/2.28e9ce1c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9f0eafda2f0415ef85c5",
+    "url": "/BOOK-REACT-READUX/static/js/main.fd9ade8c.chunk.js"
+  },
+  {
+    "revision": "f2f3b145dc39dc59ec38",
+    "url": "/BOOK-REACT-READUX/static/js/runtime-main.4597575d.js"
+  }
+]);
